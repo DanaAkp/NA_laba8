038f63f1c5bc0e7db3f2b943b9e0183dd6d975e2
Danagul Akpaeva <aidanagul10@gmail.com>
2019-04-20 16:21:44 +0300

    все правильно работает

--- NA_laba8/Program.cs
+++ NA_laba8/Program.cs
@@ -12,27 +12,104 @@ namespace NA_laba8
         {
             double a = 0;double b = 1;
             int interval = 10;
-            
-
+            //double epsilon = Math.Pow(Math.E, -3);
+            double epsilon = Math.Pow(10, -3);
+            Console.WriteLine("Left\n"+Output(calc_left(interval, epsilon, a, b), interval));
+            Console.WriteLine("Right\n" + Output(calc_right(interval, epsilon, a, b), interval));
+            Console.WriteLine("Average\n" + Output(calc_aver(interval, epsilon, a, b), interval));
+            Console.WriteLine("Trapzium\n" + Output(calc_trapezium(interval, epsilon, a, b), interval));
+            Console.WriteLine("Simpson\n" + Output(calc_Simpson(interval, epsilon, a, b), interval));
+            Console.ReadLine();
         }
         public static double Func(double x)
         {
-            return 0.37 * Math.Pow(Math.E, Math.Sin(x));
+            return x * Math.Pow(Math.E, x);
+            // return 0.37 * Math.Pow(Math.E, Math.Sin(x));
         }
-
+        public static string Output(List<double> ls, int interval)
+        {
+            string s = "";
+            for(int i = 0; i < ls.Count; i++)
+            {
+                s += (i+1).ToString()+"\t"+ interval.ToString() + "\t" + ls[i] + "\n";
+                interval *= 2;
+            }
+            return s;
+        }
         #region Вычисление погрешности
-        public static List<double> calc(int InitialInterval, double epsilon, double a, double b)
+        public static List<double> calc_left(int InitialInterval, double epsilon, double a, double b)
         {
             List<double> num = new List<double>();// буддут храниться все значения интеграла при разном количестве интервалов
             num.Add(Integral_left(InitialInterval, a, b));
-            num.Add(Integral_left(InitialInterval * 2, a, b));
+            InitialInterval *= 2;
+            num.Add(Integral_left(InitialInterval, a, b));
             double E = Math.Abs(num[num.Count - 1] - num[num.Count - 2]);
-            int c = 3;
             while (E > epsilon)
             {
-                num.Add(Integral_left(InitialInterval * c, a, b));
+                InitialInterval *= 2;
+                num.Add(Integral_left(InitialInterval, a, b));
                 E = Math.Abs(num[num.Count - 1] - num[num.Count - 2]);
-                c++;
+            }
+            return num;
+        }
+        public static List<double> calc_right(int InitialInterval, double epsilon, double a, double b)
+        {
+            List<double> num = new List<double>();// буддут храниться все значения интеграла при разном количестве интервалов
+            num.Add(Integral_right(InitialInterval, a, b));
+            InitialInterval *= 2;
+            num.Add(Integral_right(InitialInterval, a, b));
+            double E = Math.Abs(num[num.Count - 1] - num[num.Count - 2]);
+            while (E > epsilon)
+            {
+                InitialInterval *= 2;
+                num.Add(Integral_right(InitialInterval, a, b));
+                E = Math.Abs(num[num.Count - 1] - num[num.Count - 2]);
+            }
+            return num;
+        }
+        public static List<double> calc_aver(int InitialInterval, double epsilon, double a, double b)
+        {
+            List<double> num = new List<double>();// буддут храниться все значения интеграла при разном количестве интервалов
+            num.Add(Integral_aver(InitialInterval, a, b));
+            InitialInterval *= 2;
+            num.Add(Integral_aver(InitialInterval , a, b));
+            double E = Math.Abs(num[num.Count - 1] - num[num.Count - 2]);
+            while (E > epsilon)
+            {
+                InitialInterval *= 2;
+                num.Add(Integral_aver(InitialInterval , a, b));
+                E = Math.Abs(num[num.Count - 1] - num[num.Count - 2]);
+            }
+            return num;
+        }
+        public static List<double> calc_trapezium(int InitialInterval, double epsilon, double a, double b)
+        {
+            List<double> num = new List<double>();// буддут храниться все значения интеграла при разном количестве интервалов
+            num.Add(Integral_trapezium(InitialInterval, a, b));
+            InitialInterval *= 2;
+            num.Add(Integral_trapezium(InitialInterval, a, b));
+            double E = Math.Abs(num[num.Count - 1] - num[num.Count - 2]);
+            while (E > epsilon)
+            {
+                InitialInterval *= 2;
+                num.Add(Integral_trapezium(InitialInterval, a, b));
+                E = Math.Abs(num[num.Count - 1] - num[num.Count - 2]);
+              }
+            return num;
+        }
+        public static List<double> calc_Simpson(int InitialInterval, double epsilon, double a, double b)
+        {
+            List<double> num = new List<double>();// буддут храниться все значения интеграла при разном количестве интервалов
+            num.Add(Integral_Simpson(InitialInterval, a, b));
+            InitialInterval *= 2;
+            num.Add(Integral_Simpson(InitialInterval , a, b));
+            double E = Math.Abs(num[num.Count - 1] - num[num.Count - 2]);
+         
+            while (E > epsilon)
+            {
+                InitialInterval *= 2;
+                num.Add(Integral_Simpson(InitialInterval , a, b));
+                E = Math.Abs(num[num.Count - 1] - num[num.Count - 2]);
             }
             return num;
         }
@@ -42,18 +119,18 @@ namespace NA_laba8
         {
             double h = (b - a) / interval;
             double sum = 0;
-            for (int i = 0; i < interval - 1; i++)
+            for (int i = 0; i < interval; i++)
             {
                 double xi = a + i * h;
                 sum += Func(xi);
             }
             return sum * h;
         }
-        public static double Inegral_right(int interval, double a, double b)
+        public static double Integral_right(int interval, double a, double b)
         {
             double h = (b - a) / interval;
             double sum = 0;
-            for (int i = 1; i < interval; i++)
+            for (int i = 1; i < interval+1; i++)
             {
                 double xi = a + i * h;
                 sum += Func(xi);
@@ -64,7 +141,7 @@ namespace NA_laba8
         {
             double h = (b - a) / interval;
             double sum = 0;
-            for (int i = 0; i < interval - 1; i++)
+            for (int i = 0; i < interval ; i++)
             {
                 double xi = (a + (i + 1) * h - (a + i * h)) / 2;
                 sum += Func(xi);
@@ -75,7 +152,7 @@ namespace NA_laba8
         {
             double h = (b - a) / interval;
             double sum = 0;
-            for (int i = 1; i < interval - 1; i++)
+            for (int i = 1; i < interval; i++)
             {
                 double xi = a + i * h;
                 sum += Func(xi);
@@ -85,7 +162,22 @@ namespace NA_laba8
         }
         public static double Integral_Simpson(int interval, double a, double b)
         {
-
+            double h = (b - a) / interval;
+            int k = interval / 2;
+            double sum = 0;
+            for (int i = 0; i < k ; i++)
+            {
+                double xi = a + (2*i+1) * h;
+                sum += Func(xi);
+            }
+            double sum2 = 0;
+            for(int i = 1; i < k ; i++)
+            {
+                double xi = a + 2 * i * h;
+                sum2 += Func(xi);
+            }
+            sum = sum * 4 + sum2 * 2 + Func(a) + Func(b);
+            return sum * h / 3;
         }
         #endregion
     }
